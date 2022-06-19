dc381c27eaf0af222f4941e22cbff0762e592f98
ElenaShushkevich <elenashushkevich7@gmail.com>
2022-06-19 16:30:30 +0300

    add method SumFirstAndLast

--- src/com/elena/java/sumfirstandlast/SumFirstAndLast.java
+++ src/com/elena/java/sumfirstandlast/SumFirstAndLast.java
@@ -1,7 +1,25 @@
-package com.elena.java.sumfirstandlast;
+package com.elena.java.polindrom;
 
 public class SumFirstAndLast {
     public static void main(String[] args) {
-
+        System.out.println(sumFirstAndLastDigit (869));
     }
-}
+        public static int sumFirstAndLastDigit(int number) {
+            if (number < 0) {
+                return -1;
+            }
+            else if (number == 0) {
+                return 0;
+            }
+            else if (number > 0) {
+                int lastDigit = number % 10;
+
+                while (number > 9){
+                    number = number / 10;
+                }
+
+                return lastDigit + number;
+            }
+            return 0;
+        }
+    }
