cec3d0e307c09721faf925ecc3d95f282b1f9d7a
ElenaShushkevich <elenashushkevich7@gmail.com>
2022-06-19 14:31:35 +0300

    add method isPalindrome

--- src/com/elena/java/polindrom/Polindrom.java
+++ src/com/elena/java/polindrom/Polindrom.java
@@ -2,10 +2,17 @@ package com.elena.java.polindrom;
 
 public class Polindrom {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println(isPalindrome(-121121));
     }
 
-    public void testMethod(){
-
+    public static boolean isPalindrome(int number){
+        int initialNumber = Math.abs(number);
+        number = Math.abs(number);
+        int palindrom=0;
+        while(number>0){
+            palindrom = palindrom*10+number%10;
+            number = number/10;
+        }
+        return  palindrom==initialNumber;
     }
 }
