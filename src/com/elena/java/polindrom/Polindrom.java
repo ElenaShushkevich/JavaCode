fc622c04fb5a7d46f59dc3989960b724f400dddb
ElenaShushkevich <elenashushkevich7@gmail.com>
2022-06-19 14:19:53 +0300

    Add sout message into Polindrom

--- src/com/elena/java/polindrom/Polindrom.java
+++ src/com/elena/java/polindrom/Polindrom.java
@@ -2,6 +2,10 @@ package com.elena.java.polindrom;
 
 public class Polindrom {
     public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    public void testMethod(){
 
     }
 }
