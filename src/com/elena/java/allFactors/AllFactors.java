ef0be58ea07f65d7993a6286da60392fd7e05786
ElenaShushkevich <elenashushkevich7@gmail.com>
2022-06-22 23:05:22 +0300

    add method printFactors

---
+++ src/com/elena/java/allFactors/AllFactors.java
@@ -0,0 +1,21 @@
+package com.elena.java.allFactors;
+
+public class AllFactors {
+    public static void main(String[] args) {
+        printFactors(10);
+    }
+
+    public static void printFactors (int number) {
+
+        if (number>1){
+
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0){
+                    System.out.print(i +" ");;
+                }
+            }
+        } else {
+            System.out.println("Invalid Value");;
+        }
+    }
+}
