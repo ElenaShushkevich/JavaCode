2ef70de37856665b01b976441947ae3cc1ad0b8f
AlenaShushkevich <ashushkevich@suvoda.com>
2022-07-18 18:25:29 +0300

    add method InputCalculator

---
+++ src/com/elena/java/inputCalculator/InputCalculator.java
@@ -0,0 +1,31 @@
+package com.elena.java.inputCalculator;
+
+import java.util.Scanner;
+
+public class InputCalculator {
+    public static void inputThenPrintSumAndAverage(){
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        long avg = 0;
+        int count = 0;
+
+        while (true){
+            int order = count +1;
+            boolean checker = scanner.hasNextInt();
+            if (checker){
+                sum += scanner.nextInt();
+                avg = Math.round((double)(sum)/(double)(order));
+                count++;
+            }
+            else {
+                break;
+            }
+            scanner.nextLine();
+
+        }
+        System.out.println("SUM = " + sum + " AVG = " + avg);
+
+        scanner.close();
+
+    }
+}
