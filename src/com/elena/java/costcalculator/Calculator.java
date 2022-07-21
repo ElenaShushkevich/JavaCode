2d501aed041557e49f371e7d06886d5b1f470853
AlenaShushkevich <ashushkevich@suvoda.com>
2022-07-21 19:02:11 +0300

    add new classes Floor, Carpet, Calculator

---
+++ src/com/elena/java/costcalculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.elena.java.costcalculator;
+
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return floor.getArea() * carpet.getCost();
+    }
+}
